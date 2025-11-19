9abdc34fe27cb7716d6ada46eff0d0695e9de52f
iamdrzazgowski <iamdrzazgowski@gmail.com>
2025-11-19 15:22:39 +0100

    main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import HeroSection from '@/components/hero-section';
+
 export default function Home() {
-    return (
-        <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-            <h1>Hello World</h1>
-        </div>
-    );
+    return <HeroSection />;
 }
