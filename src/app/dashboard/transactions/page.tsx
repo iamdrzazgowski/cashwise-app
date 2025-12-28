39de236668bf605aab262e8b5ae788123794bdd3
iamdrzazgowski <iamdrzazgowski@gmail.com>
2025-12-28 14:21:31 +0100

    setup dialog for adding transactions

--- src/app/dashboard/transactions/page.tsx
+++ src/app/dashboard/transactions/page.tsx
@@ -1,16 +1,27 @@
+import AddTransactionDialog from '@/components/add-transaction-dialog';
 import { TransactionsTable } from '@/components/transactions-table';
 import { Card, CardContent } from '@/components/ui/card';
 
 export default function TransactionPage() {
     return (
-        <div className='min-h-screen bg-background p-4 md:p-8'>
-            <div className='mx-auto max-w-7xl space-y-6'>
-                <Card className='border-border'>
-                    <CardContent className='p-0'>
-                        <TransactionsTable />
-                    </CardContent>
-                </Card>
+        <div className='mx-auto max-w-7xl min-w-6xl space-y-6'>
+            <div className='flex items-center justify-between'>
+                <div>
+                    <h1 className='text-3xl font-bold text-foreground'>
+                        Transactions
+                    </h1>
+                    <p className='text-muted-foreground'>
+                        Manage your transactions
+                    </p>
+                </div>
+                <AddTransactionDialog />
             </div>
+
+            <Card className='border-border'>
+                <CardContent className='p-0'>
+                    <TransactionsTable />
+                </CardContent>
+            </Card>
         </div>
     );
 }
